f6ec51c6bb22376f38b099a31dac2cb5d9003a22
Emory <emory.merryman@gmail.com>
2026-02-18 20:04:55 -0500

    one second

--- alarm.js
+++ alarm.js
@@ -7,9 +7,34 @@ let alarmTimeout = null ;
 // setAlarmButton.addEventListener ( "click" , () => alert("hello victor") )
 // alarmTimeInput.addEventListener ( "dblclick" , () => alert("double clicked" ) )
 
-setAlarmButton.addEventListener ("click" , () => {
-	if(alarmTimeout) {
-		clearTimeout(alarmTimeout);
-	}
-	const timeParts = alarmTimeInput.value.split(":");
+setAlarmButton.addEventListener('click', () => {
+  if (alarmTimeout) {
+    clearTimeout(alarmTimeout);
+  }
 
+  const timeParts = alarmTimeInput.value.split(':');
+  if (timeParts.length !== 2) {
+    alert('Please enter a valid time.');
+    return;
+  }
+
+  const now = new Date();
+  const alarmTime = new Date();
+  alarmTime.setHours(parseInt(timeParts[0]));
+  alarmTime.setMinutes(parseInt(timeParts[1]));
+  alarmTime.setSeconds(0);
+
+  // If the alarm time is earlier than now, set it for tomorrow
+  if (alarmTime <= now) {
+    alarmTime.setDate(alarmTime.getDate() + 1);
+  }
+
+  const timeout = alarmTime - now;
+
+  alarmTimeout = setTimeout(() => {
+    alert('⏰ Alarm! Wake up!');
+    statusText.textContent = 'No alarm set.';
+  }, timeout);
+
+  statusText.textContent = `Alarm set for ${alarmTime.toLocaleTimeString()}`;
+});
