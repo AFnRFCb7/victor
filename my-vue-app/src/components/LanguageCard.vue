c7f2ff8875fbfb3990af73a60c97bee639cfd033
Victor Merryman <victormerryman5@gmail.com>
2026-03-17 20:40:21 -0400

    styled images

--- my-vue-app/src/components/LanguageCard.vue
+++ my-vue-app/src/components/LanguageCard.vue
@@ -15,12 +15,20 @@ const count = ref(0)
 	<tr>
 	<td>{{ name }}</td>
 	<td>{{ level }}</td>
-        <td>{{ instructor }}<img :src="picture"><img></td>		
+        <td>{{ instructor }}<img class="picture" :src="picture"><img></td>		
 	</tr>
 </template>
 
 <style scoped>
 .read-the-docs {
   color: #888;
+}
+.picture {
+      width:5cm;
+    border-radius: 50%;
+    object-fit: cover;
+    width: 5cm;
+    width: 200px;
+    height: 200px;      
 }
 </style>
