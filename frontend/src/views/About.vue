0f1562ce879ae4f9ffaebc749452c9283b68a48d
Victor Merryman <victormerryman5@gmail.com>
2026-04-03 22:18:59 -0400

    added our team still need to add image

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -3,8 +3,12 @@
     <h1>ABOUIT Page</h1>
     <p>Welcome to the ABOUT page!</p>
   </div>
+  <h1 class="chapter-title">OUR TEAM</h1>
+  <div class="team-member">
+    <div class="circle">XXXXX</div>
+    <p class="name">Lucia Liu</p>
+  </div>
 </template>
-
 <script>
 export default {
   name: 'Home'
